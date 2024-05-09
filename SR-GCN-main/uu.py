661ebb16d6e21593de82a56e2b4c86c4c33633ed
LZH101 <77476978+LZH101@users.noreply.github.com>
2024-05-09 17:07:53 +0800

    Add files via upload

---
+++ SR-GCN-main/uu.py
@@ -0,0 +1,76 @@
+import argparse
+import pickle
+import os
+
+import numpy as np
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset',
+                        required=True,
+                        choices={'ntu/xsub', 'ntu/xview', 'ntu120/xsub', 'ntu120/xset', 'NW-UCLA'},
+                        help='the work folder for storing results')
+    parser.add_argument('--alpha',
+                        default=1,
+                        help='weighted summation',
+                        type=float)
+
+    parser.add_argument('--joint-dir',
+                        help='Directory containing "epoch1_test_score.pkl" for joint eval results')
+    parser.add_argument('--bone-dir',
+                        help='Directory containing "epoch1_test_score.pkl" for bone eval results')
+    parser.add_argument('--joint-motion-dir', default=None)
+    parser.add_argument('--bone-motion-dir', default=None)
+
+    arg = parser.parse_args()
+
+    dataset = arg.dataset
+    if 'UCLA' in arg.dataset:
+        label = []
+        with open('./data/' + 'NW-UCLA/' + '/val_label.pkl', 'rb') as f:
+            data_info = pickle.load(f)
+            for index in range(len(data_info)):
+                info = data_info[index]
+                label.append(int(info['label']) - 1)
+    elif 'ntu120' in arg.dataset:
+        if 'xsub' in arg.dataset:
+            npz_data = np.load('./data/' + 'ntu120/' + 'NTU120_CSub.npz')
+            label = np.where(npz_data['y_test'] > 0)[1]
+        elif 'xset' in arg.dataset:
+            npz_data = np.load('./data/' + 'ntu120/' + 'NTU120_CSet.npz')
+            label = np.where(npz_data['y_test'] > 0)[1]
+    elif 'ntu' in arg.dataset:
+        if 'xsub' in arg.dataset:
+            npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CS.npz')
+            label = np.where(npz_data['y_test'] > 0)[1]
+        elif 'xview' in arg.dataset:
+            npz_data = np.load('./data/' + 'ntu/' + 'NTU60_CV.npz')
+            label = np.where(npz_data['y_test'] > 0)[1]
+    else:
+        raise NotImplementedError
+
+    with open(os.path.join(arg.joint_dir, 'epoch1_test_score.pkl'), 'rb') as r1:
+        r1 = list(pickle.load(r1).items())
+
+    AA = []
+    BB = []
+    for i in range(60):
+        AA.append(0)
+        BB.append(0)
+    for i in tqdm(range(len(label))):
+        l = label[i]
+#        print(l)
+        _, r11 = r1[i]
+        r = r11
+ #       print(r)
+        r = np.argmax(r)
+ #       print(r)
+        AA[l] += int(r == int(l))
+        BB[l] += 1
+    CC = []
+    for i in range(60):
+        CC.append(AA[i]/BB[i])
+    print(CC)
+    np.savetxt('a.txt',CC)
+
